22ad17f71aa7ecaf811b5996d3b88a1c31d68a1d
farshad rabiei <f.rabiei60@hotmail.com>
2020-11-14 13:58:52 +0330

    'configrabbitmq'

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RabbitMQ
+
+#install Rabbit in docker :docker pull rabbitmq:3-management
+COnfig  docker run -d -p 15672:15672 -p 5672:5672 --name rabbit-test-for-medium rabbitmq:3-management
---
+++ src/RabbitStandardQueue/RabbitFactory.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitStandardQueue
+{
+    public class RabbitFactory
+    {
+
+        private static IConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        public static void CreateQueue()
+        {
+
+
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+        }
+
+    }
+}
