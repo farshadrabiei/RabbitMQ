8a0cc80dc13213794461f2ea388865edf7bee12e
farshad rabiei <f.rabiei60@hotmail.com>
2020-11-16 08:49:20 +0330

    Complete Simple Message

--- src/RabbitStandardQueue/RabbitFactory.cs
+++ src/RabbitStandardQueue/RabbitFactory.cs
@@ -1,7 +1,11 @@
-﻿using RabbitMQ.Client;
+﻿using Common;
+using Newtonsoft.Json.Converters;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace RabbitStandardQueue
 {
@@ -11,13 +15,37 @@ namespace RabbitStandardQueue
         private static IConnectionFactory _factory;
         private static IConnection _connection;
         private static IModel _model;
+        private static string QueueName = "StandardQueue_ExampleQueue";
 
-        public static void CreateQueue()
+        public   void CreateQueue()
         {
-
-
             _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
         }
 
+
+        public   void SendMessage(Payment message) 
+        {
+            _model.BasicPublish("", QueueName, null, message.Serialize());
+            Console.WriteLine(" [x] Payment Message Sent : {0} : {1} : {2}", message.CardNumber, message.AmountToPay, message.Name);
+
+        }
+        public void Recieve()
+        {
+            EventingBasicConsumer consumer = new EventingBasicConsumer(_model);
+
+            consumer.Received += (o, e) => {
+                Payment data =  (Payment)e.Body.ToArray().DeSerialize(typeof(Payment));
+                Console.WriteLine($"received: AmountToPay : {data.AmountToPay} , CardNumber : {data.CardNumber} , Name :{data.Name}" );
+                Thread.Sleep(1000);
+                Console.WriteLine("done."); //Received event will not be fired again before done.
+            };
+            _model.BasicConsume(queue: QueueName,
+                                autoAck: true,
+                                consumer: consumer);
+
+        }
     }
 }
--- src/StandardQueue/Program.cs
+++ src/StandardQueue/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Common;
+using RabbitStandardQueue;
+using System;
+using System.Threading;
 
 namespace StandardQueue
 {
@@ -6,7 +9,22 @@ namespace StandardQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RabbitFactory rabbitFactory = new RabbitFactory();
+
+               var payment1 = new Payment { AmountToPay = 25.0m, CardNumber = "1234123412341234", Name = "Mr S Hants 1" };
+            var payment2 = new Payment { AmountToPay = 2.0m, CardNumber = "1234123412341234", Name = "Mr S Hants 2" };
+            var payment3 = new Payment { AmountToPay =  5.0m, CardNumber = "1234123412341234", Name = "Mr S Hants 3" };
+            var payment4 = new Payment { AmountToPay = 236.0m, CardNumber = "1234123412341234", Name = "Mr S Hants 4" };
+
+            rabbitFactory.CreateQueue();
+            rabbitFactory.SendMessage(payment1);
+            rabbitFactory.SendMessage(payment2);
+            rabbitFactory.SendMessage(payment3);
+            rabbitFactory.SendMessage(payment4);
+
+            Thread.Sleep(10000);
+            rabbitFactory.Recieve();
+            Console.ReadKey();
         }
     }
 }
